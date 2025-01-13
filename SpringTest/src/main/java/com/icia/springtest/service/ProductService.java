cf0d388b66b2cbc8f3003816fcdfbb1fe0001ce8
alsdud0301 <jm931017@gmail.com>
2025-01-13 19:34:30 +0900

    페이징 기능 추가

--- SpringTest/src/main/java/com/icia/springtest/service/ProductService.java
+++ SpringTest/src/main/java/com/icia/springtest/service/ProductService.java
@@ -2,23 +2,35 @@ package com.icia.springtest.service;
 
 import com.icia.springtest.dao.ProductDao;
 import com.icia.springtest.dto.ProductDto;
+import com.icia.springtest.dto.SearchDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
     @Autowired
     private ProductDao pDao;
-    public List<ProductDto> getProductList(ProductDto pDto) {
-//        Map<String,Integer> pageMap = new HashMap<>();
-//        pageMap.put("startIndex")
 
-        return pDao.getProductList(pDto);
+    public List<ProductDto> getProductList(SearchDto sDto) {
+        if(sDto.getPageNum()!=null){
+            log.info("서비스 실행중");
+                if(sDto.getPageNum()>0){
+                    sDto.setEnd(10);
+                   sDto.setStart(sDto.getEnd()*(sDto.getPageNum()-1));
+                }
+
+
+        }
+        return pDao.getProductList(sDto);
+    }
+
+    public boolean insertProduct(ProductDto pDto) {
+        return pDao.insertProduct(pDto);
     }
 }
